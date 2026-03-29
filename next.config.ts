ea5f3f24ade9df582fe51b3afe4b84db17f9008a
Anurag Borkar <anuragborkar2005@gmail.com>
2026-03-29 12:48:00 +0000

    Config Reown SDK

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  //@ts-expect-error Due to config any type
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
